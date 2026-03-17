c2e2b5707cd37b33247b12ec04892a7c4b1f33e4
leeyeonjoo123 <yjlee22501@gmail.com>
2026-03-17 18:57:40 +0900

    Initial Commit

---
+++ MyDayTimer/MyDayTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDayTimerApp.swift
+//  MyDayTimer
+//
+//  Created by 1234 on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MyDayTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
